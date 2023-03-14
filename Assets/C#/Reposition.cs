d2bbb67047ac18d90e745f46b54244304127ff0d
Park Yeon Woo <jd05183@naver.com>
2023-03-15 02:12:38 +0900

    230315_update_01
    +Enemy스크립트
    
    Update
    reposition스크립트

--- Assets/C#/Reposition.cs
+++ Assets/C#/Reposition.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake(){
+        coll = GetComponent<Collider2D>();
+    }
+
     void OnTriggerExit2D(Collider2D collision){
         if(!collision.CompareTag("Area"))
             return;
@@ -26,6 +32,11 @@ public class Reposition : MonoBehaviour
                 }
                 else if(diffX < diffY){
                     transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+            case "Enemy":
+                if(coll.enabled){
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f,3f), Random.Range(-3f,3f), 0f));
                 }
                 break;
         }

f4d7b6bb8d760985d6928923503086252aac8a1b
Park Yeon Woo <jd05183@naver.com>
2023-03-20 22:21:25 +0900

    230320_update_01
    +weapon01, scanner

--- Assets/C#/Weapon.cs
+++ Assets/C#/Weapon.cs
@@ -10,6 +10,13 @@ public class Weapon : MonoBehaviour
     public int count;
     public float speed;
 
+    float timer;
+    Player player;
+
+    void Awake(){
+        player = GetComponentInParent<Player>();
+    }
+
     void Start(){
         Init();
     }
@@ -20,12 +27,18 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if(timer > speed){
+                    timer = 0f;
+                    Fire();
+                }
                 break;
         }
 
         //test
         if(Input.GetButtonDown("Jump")){
-            LevelUp(20, 5);
+            LevelUp(10, 1);
         }
         
     }
@@ -45,6 +58,7 @@ public class Weapon : MonoBehaviour
                 Pos1();
                 break;
             default:
+                speed = 0.3f;
                 break;
         }
     }
@@ -66,7 +80,22 @@ public class Weapon : MonoBehaviour
             Vector3 rotVec = Vector3.forward * 360 * i / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
-            bullet.GetComponent<Bullet>().Init(damage, -1);   // -1 = Inf   
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero);   // -1 = Inf   
         }  
+    }
+
+    void Fire(){
+        if(!player.scanner.nearestTarget){
+            return;
+        }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+            
+        Transform bullet = GameManager.instance.pm.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);   
     }
 }

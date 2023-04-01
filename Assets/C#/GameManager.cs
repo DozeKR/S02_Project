e37e24c7a02127d41d7c516979d1e1d69fb420d5
Park Yeon Woo <jd05183@naver.com>
2023-04-01 13:15:28 +0900

    230401_update_02
    +HUD

--- Assets/C#/GameManager.cs
+++ Assets/C#/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
     public float maxGameTime = 2 * 10f;
 
     [Header("# Player Info")]
+    public int health;
+    public int maxHealth = 100;
     public int level;
     public int kill;
     public int exp;
@@ -21,6 +23,10 @@ public class GameManager : MonoBehaviour
 
     void Awake(){
         instance = this;
+    }
+
+    void Start(){
+        health = maxHealth;
     }
 
     void Update(){
